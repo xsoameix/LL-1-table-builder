3b6dc959bce06b16d9e142e3acc058b7f43fbb50
廉強 <xsoameix@gmail.com>
2013-04-07 06:05:53 +0800

    Improve the scan func readability by writting some comparison funcs.

--- app/Scanner.c
+++ app/Scanner.c
@@ -11,22 +11,22 @@ Token* scan(char *string) {
         }
 
         int type;
-        if(strncmp(&string[forward], "->", 2) == 0) { // "->" == 2 chars
+        if(isArrow(string, forward)) {
                 type = ARROW;
                 forward += 2; // "->" == 2 chars
-        } else if(string[forward] == '|') {
+        } else if(isOr(string, forward)) {
                 type = OR;
                 forward++;
-        } else if(isalpha(string[forward]) || ispunct(string[forward])) {
-                if(forward == 0 || string[forward - 1] == 0x0A || string[forward - 1] == 0x0D) {
+        } else if(isAlphaOrPunct(string, forward)) {
+                if(isAtNewLine(string, forward)) {
                         type = NT;
                 } else {
                         type = TOKEN;
                 }
-                while(isalpha(string[forward]) || ispunct(string[forward])) {
+                while(isAlphaOrPunct(string, forward)) {
                         forward++;
                 }
-        } else if(string[lexemeBegin] == '\0') {
+        } else if(isEOF(string, lexemeBegin)) {
                 return NULL;
         }
 
@@ -36,4 +36,24 @@ Token* scan(char *string) {
 
         lexemeBegin = forward;
         return token;
+}
+
+bool isArrow(char *s, int offset) {
+        return strncmp(&s[offset], "->", 2) == 0; // "->" == 2 chars
+}
+
+bool isOr(char *s, int offset) {
+        return s[offset] == '|';
+}
+
+bool isAlphaOrPunct(char *s, int offset) {
+        return isalpha(s[offset]) || ispunct(s[offset]);
+}
+
+bool isAtNewLine(char *s, int offset) {
+        return offset == 0 || s[offset - 1] == 0x0A || s[offset - 1] == 0x0D;
+}
+
+bool isEOF(char *s, int offset) {
+        return s[offset] == '\0';
 }
--- app/Scanner.h
+++ app/Scanner.h
@@ -3,9 +3,15 @@
 
 #include <ctype.h>
 #include <string.h>
+#include <stdbool.h>
 #include "Lib.h"
 #include "Token.h"
 
 extern Token* scan(char *string);
+extern bool isArrow(char *s, int offset);
+extern bool isOr(char *s, int offset);
+extern bool isAlphaOrPunct(char *s, int offset);
+extern bool isAtNewLine(char *s, int offset);
+extern bool isEOF(char *s, int offset);
 
 #endif
